12be49824fbfa029821a91ce9de5b84bd724846b
fendou666 <ls_code@126.com>
2017-06-24 07:01:21 +0800

    万能分页函数

--- 20170617项目整理.sql
+++ 20170617项目整理.sql
@@ -133,7 +133,43 @@ FROM CLASS c WHERE c.class_id=20170207;
 
 
 
-
+select 
+           class_Id,
+				   EEC_Id, 
+				   EEC_Name,
+				   sex,
+				   age,
+				   email,
+				   telephone,
+				   role_name 
+              from 
+              (
+              select 
+                   case when mod(Lineno,10) =0 then Lineno/10
+                        when mod(Lineno,10)!=0 then trunc(Lineno/10)+1 end as pageno,
+                        class_Id,
+                        EEC_Id, 
+                        EEC_Name,
+                        sex,
+                        age,
+                        email,
+                        telephone,
+                        role_name
+              from
+                (
+                  select row_number() over(order by  u.EEC_Id)  as Lineno,
+                         u.class_Id,
+						 u.EEC_Id,
+						 u.EEC_Name,
+						 u.sex,
+						 u.age,
+						 u.email,
+						 u.telephone,
+						 r.role_name  
+                    from eecuser u, eecrole r 
+                         where u.role_id = r.role_id and u.role_id >= 3004 AND u.role_id <= 3005) ss 
+                ) 
+            where pageno=1;
 
 
 
--- 20170623万能分页.sql
+++ 20170623万能分页.sql
@@ -4,34 +4,68 @@
 需要count字段
 
 ---万能分页函数
-create or replace function eecAdminQueryTeacherPageRows(v_maxPageRows in number,
+create or replace function eecQueryPageRows(v_maxPageRows in number,
                             v_currentPageNumber in number,
-                            v_getCountFild      in varchar2, --eec_id
-                            v_fildAll           in varchar2, --
-                            v_tableAll          in varchar2, --eecuser u, eecrole r
+                            v_sqlCountFild      in varchar2, --eec_id
+                            v_sqlOrderByFild    in varchar2, --eec_id
                             v_condition         in varchar2, -- condition字段
+                            v_sqlAliasNeedFilds in varchar2, --
+                            v_sqlAliasTableStr  in varchar2, --eecuser u, eecrole r
+                            v_sqlNeedFilds      in varchar2, --
+                            v_sqlTableStr       in varchar2, --eecuser u, eecrole r
                             v_count             out number)
     return sys_refcursor as
-    v_getCountSql   varchar2(200) := 'select count() from  where ' || v_condition;
-    v_getPageRecord varchar2(2000) := 'select sss.class_Id, sss.EEC_Id, sss.EEC_Name, sss.sex, sss.age, sss.email, sss.telephone, sss.role_name 
+    v_getCountSql   varchar2(200) := 'select count('|| v_sqlCountFild ||') from '|| v_sqlAliasTableStr ||' where ' || v_condition;
+    v_getPageRecord varchar2(2000) := 'select ' || v_sqlNeedFilds ||' 
           from  (  select 
-           case when mod(ss.Lineno,:maxPageRows) =0 then ss.Lineno/:maxPageRows
-            when mod(ss.Lineno,:maxPageRows)!=0 then trunc(ss.Lineno/:maxPageRows)+1 end as pageno,
-            ss.class_Id,ss.EEC_Id,  ss.EEC_Name, ss.sex, ss.age, ss.email, ss.telephone, ss.role_name
-          from  (select row_number() over(order by  u.EEC_Id)  as Lineno,
-             u.class_Id, u.EEC_Id, u.EEC_Name, u.sex,u.age, u.email, u.telephone, r.role_name  
-            from eecuser u, eecrole r where ' ||
+           case when mod(Lineno,:maxPageRows) =0 then Lineno/:maxPageRows
+            when mod(Lineno,:maxPageRows)!=0 then trunc(Lineno/:maxPageRows)+1 end as pageno,
+            ' || v_sqlNeedFilds ||'
+          from  (select row_number() over(order by  '|| v_sqlOrderByFild ||')  as Lineno,
+             '|| v_sqlAliasNeedFilds ||'  
+            from ' || v_sqlAliasTableStr ||' where ' ||
                       v_condition ||
-                      ' )  ss 
-        ) sss 
-        where sss.pageno=:currentPageNumbe';
+                      ' )
+        )
+        where pageno=:currentPageNumbe';
     myPagecur       sys_refcursor;
 begin
-    EXECUTE IMMEDIATE v_getCountSql
-        INTO v_count;
+    --Dbms_Output.put_line('v_getCountSql:' || v_getCountSql);
+    --Dbms_Output.put_line('v_getPageRecord:' || v_getPageRecord);
+    EXECUTE IMMEDIATE v_getCountSql INTO v_count;
     Dbms_Output.put_line('v_count:' || v_count);
     open myPagecur for v_getPageRecord
         using v_maxPageRows, v_maxPageRows, v_maxPageRows, v_maxPageRows, v_currentPageNumber;
     return myPagecur;
 end;
+
+
+
+
+
+
+create or replace function eecTestCount(
+                            v_sqlCountFild      in varchar2, --eec_id
+                            v_condition         in varchar2, 
+                            v_sqlAliasTableStr  in varchar2, --eecuser u, eecrole r
+                            v_count             out number)
+    return sys_refcursor as
+    v_getCountSql   varchar2(200) := 'select count('|| v_sqlCountFild ||') from '|| v_sqlAliasTableStr ||' where ' || v_condition;
+    myPagecur       sys_refcursor;
+begin
+    Dbms_Output.put_line('v_getCountSql:' || v_getCountSql);
+    --Dbms_Output.put_line('v_getPageRecord:' || v_getPageRecord);
+    EXECUTE IMMEDIATE v_getCountSql INTO v_count;
+    open myPagecur for select * from emp;
+    return myPagecur;
+end;
+
+
+
+
+
+
+
+
+
 
