d294f88d9d0068267dfd2597214c785ad00b373e
fendou666 <ls_code@126.com>
2017-07-07 18:19:09 +0800

    添加测试

--- 2070703TestJT.sql
+++ 2070703TestJT.sql
@@ -23,3 +23,30 @@ begin
     open myCursor for SELECT empno, ename FROM emp WHERE empno=7369;
     return myCursor;
 end;
+create or replace function testJT2(v_inparam in number,
+                            v_count     out number,
+                            v_str in out varchar2)
+return number 
+as
+    myCursor  number;
+begin
+    Dbms_Output.put_line('in params'  || v_inparam);
+    Dbms_Output.put_line('inout params'  || v_str);
+    v_count:=11;
+    v_str:='我是sql inout的赋值';
+    return v_count;
+end;
+
+
+
+
+create or replace procedure testJTPro(v_inparam in number,
+                            v_count     out number,
+                            v_str in out varchar2)
+as
+   
+begin
+    Dbms_Output.put_line('in params'  || v_inparam);
+    v_count:=11+v_inparam;
+    v_str:='我是sql inout的赋值';
+end;
